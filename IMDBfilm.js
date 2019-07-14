5badb29064efaa262d4e3b0cb145dbf0c4cfa6e8
Adil Dev <adilbekmomunaliev@Adilbeks-MacBook-Pro.local>
2019-07-14 15:08:10 -0700

    config created

--- IMDBfilm.js
+++ IMDBfilm.js
@@ -1,10 +1,7 @@
-/* 
-Inifial test design: make a person to search for a film by entering 
-keyword into the URL address bar. Later will be wraped into some
-front end and have a entry bar for that.
-*/
+
 const express = require('express');
 const request = require('request');
+const config = require('./config.json');
 
 const app = express()
 var server = app.listen(3000, listening);
@@ -14,13 +11,13 @@ function listening() {
 }
 // root page
 app.use(express.static("website"));
+app.get('/:keyWord', getFilmData);
 
- app.get('/:keyWord', getFilmData);
  function getFilmData(req, res) {
 
  	var keyWord = req.params.keyWord;
  	//call OMDB API using request
- 	request('http://www.omdbapi.com/?s='+keyWord+'&apikey=aa200f33', { json: true }, (err, resp, body) => {
+ 	request('http://www.omdbapi.com/?s='+keyWord+'&apikey='+config.APIKey, { json: true }, (err, resp, body) => {
  		 if (err) { return console.log(err); }
   		res.send(body.Search);
 	});
